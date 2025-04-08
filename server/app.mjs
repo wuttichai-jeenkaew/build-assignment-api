2e25c85b33db40ad78eeda527cb154c0c1c2c8cc
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-04-08 16:06:27 +0700

    assignment

--- server/app.mjs
+++ server/app.mjs
@@ -1,12 +1,49 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
+
+app.use(express.json())
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
 
+app.post("/assignments", async (req, res) => {
+  const newAssignment = {
+    ...req.body,
+    created_at: new Date(),
+    updated_at: new Date(),
+    published_at: new Date(),
+  };
+
+  try {
+    await connectionPool.query(
+      `insert into assignments (user_id, title, content, category, created_at, updated_at, published_at)
+      values ($1, $2, $3, $4, $5, $6, $7)`,
+      [
+        1,
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at,
+      ]
+    );
+  } catch(error) {
+    return res.status(500).json({
+      error: error.message,
+      message: "Server could not create assignment because database connection",
+    });
+  }
+
+
+  return res.status(201).json({ message: "Created assignment sucessfully" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
+
--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString:
-    "postgresql://your-db-username:your-db-password@localhost:5432/your-db-name",
+    "postgresql://postgres:123456@localhost:5432/KKK",
 });
 
 export default connectionPool;
